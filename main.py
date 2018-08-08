b17146eedc06db672e364aba318e16e9bde35393
Ras Gabriel Hipolito <wasitoras@gmail.com>
2018-08-08 16:39:34 +0000

    fix

--- main.py
+++ main.py
@@ -78,9 +78,4 @@ app = webapp2.WSGIApplication([
     ('/', EnterInfoHandler),
     ('/showrecipe', ShowRecipeHandler),
     ('/allrecipes', AllRecipesHandler)
-<<<<<<< HEAD
 ], debug=True)
-=======
-], debug=True)
-
->>>>>>> 19d22874f9d3db39a425407f83440b2c5c1448a9
